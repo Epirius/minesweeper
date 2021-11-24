394e98803125ee7f4afb9d635325cfeced475ac2
Epirius <68005825+Epirius@users.noreply.github.com>
2021-11-25 00:31:50 +0100

    added mines, and numbers adjacent to the mines. added a floodfill algo that shows all connected empty squares.

--- board.py
+++ board.py
@@ -4,18 +4,18 @@ from random import randrange
 
 class Board:
 	# you only need to use update_board() outside this class.
-	# the board size has to be a multiple of 3.
 
 	def __init__(self, size, mine_numbers):
 		self.size = size
 		self.board_data = self.create_board(self.size)
-		self.mine_positions = self.generate_mines(mine_numbers)
+		self.mine_board = self.generate_mine_board(mine_numbers)
 
 	def create_board(self, size):
 		board_data = []
 		for x in range(size):
 			row = []
 			for y in range(size):
+				#this is the "default" icon.
 				row.append(" ◌ ")
 			board_data.append(row)
 		return board_data
@@ -60,26 +60,104 @@ class Board:
 		mine_list = []
 
 		while len(mine_list) < mine_numbers:
-			x = randrange(1, self.size + 1)
-			y = randrange(1, self.size + 1)
+			x = randrange(self.size)
+			y = randrange(self.size)
 
 			if (x, y) not in mine_list:
 				mine_list.append((x, y))
-
 		return mine_list
+
+	def generate_mine_board(self, mine_numbers):
+		mine_list = self.generate_mines(mine_numbers)
+		mine_board = []
+		for x in range(self.size):
+			line = []
+			for y in range(self.size):
+				line.append(0)
+			mine_board.append(line)
+
+		for mine in mine_list:
+			#add a mine to the mine position, and add 1 to all adjecent spots
+			x = mine[0]
+			y = mine[1]
+			mine_board[y][x] = "◉"  # negative = mine
+
+			try:
+				mine_board[y - 1][x - 1] += 1
+			except Exception:
+				pass
+			try:
+				mine_board[y - 1][x + 1] += 1
+			except Exception:
+				pass
+			try:
+				mine_board[y - 1][x] += 1
+			except Exception:
+				pass
+
+			try:
+				mine_board[y][x - 1] += 1
+			except Exception:
+				pass
+			try:
+				mine_board[y][x + 1] += 1
+			except Exception:
+				pass
+
+			try:
+				mine_board[y + 1][x - 1] += 1
+			except Exception:
+				pass
+			try:
+				mine_board[y + 1][x + 1] += 1
+			except Exception:
+				pass
+			try:
+				mine_board[y + 1][x] += 1
+			except Exception:
+				pass
+
+		return mine_board
+
+	def flood_fill(self, x, y):
+		if self.board_data[y][x] == " ◌ " and self.mine_board[y][x] == "◉":
+			raise Exception("the flood fill algo hit a mine, but it shouldn't because it will stop when it hits a number.")
+
+		elif self.board_data[y][x] == " ◌ " and self.mine_board[y][x] > 0:
+			self.board_data[y][x] = " " + str(self.mine_board[y][x]) + " "
+
+		elif self.board_data[y][x] == " ◌ " and self.mine_board[y][x] == 0:
+			self.board_data[y][x] = "   "
+			try:
+				if x > 0:
+					self.flood_fill(x - 1, y)
+				if x < self.size:
+					self.flood_fill(x + 1, y)
+				if y > 0:
+					self.flood_fill(x, y - 1)
+				if y < self.size:
+					self.flood_fill(x, y + 1)
+			except:
+				pass
 
 	def update_board(self, position):
 		"""Takes position [x,y] as input
 			returns a updated board as a string
 		"""
 
-		mine = " ◉ "
-		empty = "   "
-
 		x = position[0] - 1
 		y = position[1] - 1
 
-		# adding the player icon to the board
-		self.board_data[x][y] = mine
+		if self.mine_board[y][x] == "◉":
+			self.board_data[y][x] = " ◉ "
+			return False
+
+		elif isinstance(self.mine_board[y][x], int) and self.mine_board[y][x] > 0:
+			self.board_data[y][x] = " " + str(self.mine_board[y][x]) + " "
+
+		else:
+			self.flood_fill(x, y)
+
+		print(self.mine_board)
 
 		return self.draw_board()
--- minesweeper.py
+++ minesweeper.py
@@ -1,14 +1,6 @@
 from board import Board
 
-board = Board(15, 10)
+board = Board(12, 10)
 # print(board.draw_board())
-board.update_board((2, 5))
-board.update_board((13, 13))
-board.update_board((15, 2))
-board.update_board((12, 3))
-board.update_board((13, 4))
-board.update_board((9, 3))
-board.update_board((7, 9))
-board.update_board((3, 11))
-board.update_board((11, 4))
-print(board.update_board((1, 8)))
+
+print(board.update_board((2, 5)))
